57a43404a766dc8b5c1e1b738bcf7ec84afbb331
daniela botelho <daniela.botelho@venturus.org.br>
2022-03-29 13:13:35 -0300

    desafio 02

--- src/App.tsx
+++ src/App.tsx
@@ -19,23 +19,13 @@ interface GenreResponseProps {
   title: string;
 }
 
-interface MovieProps {
-  imdbID: string;
-  Title: string;
-  Poster: string;
-  Ratings: Array<{
-    Source: string;
-    Value: string;
-  }>;
-  Runtime: string;
-}
+
 
 export function App() {
-  const [selectedGenreId, setSelectedGenreId] = useState(1);
-
+  
   const [genres, setGenres] = useState<GenreResponseProps[]>([]);
 
-  const [movies, setMovies] = useState<MovieProps[]>([]);
+  
   const [selectedGenre, setSelectedGenre] = useState<GenreResponseProps>({} as GenreResponseProps);
 
   useEffect(() => {
@@ -44,15 +34,7 @@ export function App() {
     });
   }, []);
 
-  useEffect(() => {
-    api.get<MovieProps[]>(`movies/?Genre_id=${selectedGenreId}`).then(response => {
-      setMovies(response.data);
-    });
-
-    api.get<GenreResponseProps>(`genres/${selectedGenreId}`).then(response => {
-      setSelectedGenre(response.data);
-    })
-  }, [selectedGenreId]);
+ 
 
   function handleClickButton(id: number) {
     setSelectedGenreId(id);
